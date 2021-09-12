c2c253b1440f38bcc5c6ba03938040a28702eab9
sluongo718 <sluongo718@yahoo.com>
2021-09-12 00:22:12 -0400

    commit tree 66500daa2836c6d285e43da5ce295b8013f074c5

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,42 @@
 // add code snippets from README
+function createStore(reducer) {
+    let state;
+  
+    function dispatch(action) {
+      state = reducer(state, action);
+      render();
+    }
+  
+    function getState() {
+      return state;
+    };
+  
+    return {
+      dispatch,
+      getState
+    };
+  };
+  
+  function reducer(state = { count: 0 }, action) {
+    switch (action.type) {
+      case 'INCREASE_COUNT':
+        return { count: state.count + 1 };
+  
+      default:
+        return state;
+    }
+  }
+  
+  
+  function render() {
+    let container = document.getElementById('container');
+    container.textContent = store.getState().count;
+  };
+  
+  let store = createStore(reducer) // createStore takes the reducer as an argument
+  store.dispatch({ type: '@@INIT' });
+  let button = document.getElementById('button');
+  
+  button.addEventListener('click', () => {
+    store.dispatch({ type: 'INCREASE_COUNT' });
+  });
